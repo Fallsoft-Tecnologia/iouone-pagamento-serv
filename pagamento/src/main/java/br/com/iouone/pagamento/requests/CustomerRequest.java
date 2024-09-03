98a30ae71e39acccf5bd625ddad141f3ff1ccca5
Hebert Soares <hebertsoaresof@gmail.com>
2024-09-03 14:29:31 -0300

    :sparkles: feat: adicionar campo `id` em CustomerRequest e enviar CustomerId para o microservice de Cliente
    - Adicionado o campo `id` à classe `CustomerRequest` para armazenar o identificador da pessoa.
    - Implementados getters e setters para o campo `id`.
    - Injetado `RabbitTemplate` no `ClienteService` para envio de mensagens.
    - Implementada lógica para enviar `CustomerIdMessageRequest` para a fila `receiving_customer_id_queue` após a criação do cliente no Pagar.me.
    - Adicionadas mensagens de log para monitorar o envio do ID de cliente.

--- pagamento/src/main/java/br/com/iouone/pagamento/requests/CustomerRequest.java
+++ pagamento/src/main/java/br/com/iouone/pagamento/requests/CustomerRequest.java
@@ -1,6 +1,8 @@
 package br.com.iouone.pagamento.requests;
 
 public class CustomerRequest {
+
+    private Integer id;
     private String name;
     private String email;
     private String document;
@@ -20,6 +22,14 @@ public class CustomerRequest {
         this.gender = gender;
         this.birthdate = birthdate;
         this.celular = celular;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
